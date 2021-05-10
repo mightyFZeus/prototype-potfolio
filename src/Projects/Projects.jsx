8079cb971f50d02f95ceed4c12b779b22890889a
mightyFZeus <67365597+mightyFZeus@users.noreply.github.com>
2021-05-11 06:41:11 +0800

    products

---
+++ src/Projects/Projects.jsx
@@ -0,0 +1,42 @@
+import React from "react";
+import { Card, Grid, CardContent, Typography, Button } from "@material-ui/core";
+
+const Projects = ({ resume }) => {
+  return (
+    <>
+      <Grid container justify="center" spacing={4}>
+        {resume.map((resume) => (
+          <Grid item key={resume.id} xs={12} sm={6} md={4} lg={3}>
+            <Card>
+              <CardContent key={resume.id}>
+                <Typography>{resume.title}</Typography>
+                <Typography>{resume.stack}</Typography>
+                <Typography>{resume.description}</Typography>
+                <a href={resume.github}>
+                  <Button
+                    style={{ textTransform: "none" }}
+                    variant="contained"
+                    color="primary"
+                  >
+                    github
+                  </Button>
+                </a>
+                <a href={resume.live}>
+                  <Button
+                    style={{ textTransform: "none" }}
+                    variant="contained"
+                    color="secondary"
+                  >
+                    live
+                  </Button>
+                </a>
+              </CardContent>
+            </Card>
+          </Grid>
+        ))}
+      </Grid>
+    </>
+  );
+};
+
+export default Projects;
