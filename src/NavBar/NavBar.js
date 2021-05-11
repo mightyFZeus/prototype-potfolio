6f9b596228a7e9b7baca67812ce5edd253ba648b
mightyFZeus <67365597+mightyFZeus@users.noreply.github.com>
2021-05-11 21:00:44 +0800

    drawer

--- src/NavBar/NavBar.js
+++ src/NavBar/NavBar.js
@@ -1,27 +1,48 @@
-import React from 'react'
-import {AppBar, Toolbar, IconButton, Typography} from '@material-ui/core'
-import MenuIcon from '@material-ui/icons/Menu';
-import useStyles from './Style'
-const NavBar =() =>{
-  const classes = useStyles()
-  return(
+import React, { useState } from "react";
+import {
+  AppBar,
+  Toolbar,
+  IconButton,
+  Typography,
+  Drawer,
+  List,
+  ListItem,
+  ListItemText
+} from "@material-ui/core";
+import MenuIcon from "@material-ui/icons/Menu";
+import useStyles from "./Style";
+const NavBar = () => {
+  const classes = useStyles();
+  const [drawer, setDrawer] = useState(false);
+  const toggleDrawer = (open) => (event) => setDrawer(open);
+
+  const test = () => (
+    <List>
+      <ListItem> it works</ListItem>
+    </List>
+  );
+  return (
     <>
-      <AppBar position='fixed'  color='inherit'>
-        <Toolbar >
-          <Typography variant='h6'>
-            +Zeus
-          </Typography>
-          
-            <div className={classes.menu} >
-            <IconButton >
+      <AppBar position="fixed" color="inherit">
+        <Toolbar>
+          <Typography variant="h6">+Zeus</Typography>
+
+          <div className={classes.menu}>
+            <IconButton onClick={toggleDrawer(true)}>
               <MenuIcon />
             </IconButton>
-            </div>
-          
+            <Drawer
+              anchor={"right"}
+              open={drawer}
+              onClose={toggleDrawer(false)}
+            >
+              {test()}
+            </Drawer>
+          </div>
         </Toolbar>
       </AppBar>
     </>
-  )
-}
+  );
+};
 
-export default NavBar
+export default NavBar;
