002948dfbf2df1f753943782de5336e71409d743
mightyFZeus <67365597+mightyFZeus@users.noreply.github.com>
2021-05-12 19:52:50 +0800

    x

--- src/Home/Home.jsx
+++ src/Home/Home.jsx
@@ -2,12 +2,15 @@ import React from "react";
 import { Typography } from "@material-ui/core";
 import theme from "./Styles";
 import "./Styles.css";
+import useStyles from "./Styles2";
 import { ThemeProvider } from "@material-ui/core/styles";
 
 const Home = () => {
+  const classes = useStyles();
   return (
     <>
       <ThemeProvider theme={theme}>
+        <div className={classes.toolbar} />
         <Typography variant="h2">this is just a test </Typography>
       </ThemeProvider>
     </>
