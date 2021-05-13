007d9337879a0323a0e95ece43a677ea31535eeb
mightyFZeus <67365597+mightyFZeus@users.noreply.github.com>
2021-05-14 05:46:55 +0800

    desktop done

--- src/Home/Home.jsx
+++ src/Home/Home.jsx
@@ -1,12 +1,26 @@
-import React from "react";
+import React,{useEffect, useState} from "react";
 import Desktop from './Desktop/Desktop'
 
 
 const Home = ({resume}) => {
-  ;
+  const [isDesktop, setDesktop] = useState(window.innerWidth > 900);
+  
+  const updateMedia = () => {
+    setDesktop(window.innerWidth > 900);
+  };
+
+  useEffect(() => {
+    window.addEventListener("resize", updateMedia);
+    return () => window.removeEventListener("resize", updateMedia);
+  });
+
   return (
     <>
-        <Desktop resume={resume} />
+        <div>
+      {isDesktop ? <Desktop resume={resume} /> : (
+        <div>I show on 1450px or lower</div>
+      )}
+    </div>
     </>
   );
 };
