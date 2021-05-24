e6b107c50f7d945a7478e8f8b842fad270aa5392
mightyFZeus <67365597+mightyFZeus@users.noreply.github.com>
2021-05-24 08:03:47 +0800

    still contact

--- src/App.js
+++ src/App.js
@@ -73,7 +73,7 @@ export default function App() {
               <Route exact path="/Projects">
                 <Projects resume={resume} />
               </Route>
-              <Route exact path="/Contact">
+              <Route exact path="/Contacts">
                 <Contact />
               </Route>
             </Switch>
--- src/Contact/Contact.jsx
+++ src/Contact/Contact.jsx
@@ -1,9 +1,35 @@
 import React from "react";
-
+import emailjs from "emailjs-com";
+import useStyles from "./Styles";
 const Contact = () => {
+  const classes = useStyles();
+  function sendEmail(e) {
+    e.preventDefault();
+
+    emailjs
+      .sendForm("YOUR_SERVICE_ID", "YOUR_TEMPLATE_ID", e.target, "YOUR_USER_ID")
+      .then(
+        (result) => {
+          console.log(result.text);
+        },
+        (error) => {
+          console.log(error.text);
+        }
+      );
+  }
+
   return (
     <>
-      <h1 style={{ color: "white" }}>this is a Contact form</h1>
+      <div className={classes.toolbar} />
+      <form className="contact-form" onSubmit={sendEmail}>
+        <label>Name</label>
+        <input type="text" name="name" />
+        <label>Email</label>
+        <input type="email" name="email" />
+        <label>Message</label>
+        <textarea name="message" />
+        <input type="submit" value="Send" />
+      </form>
     </>
   );
 };
